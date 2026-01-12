34f7aa64c89d701d39f1401ef6e3c7e5a8cacb98
Amélie Noel <amelie.loiseaunoel@gmail.com>
2026-01-12 15:21:12 +0000

    Merge tables (raw yt, raw spotify + genre tables) for history intermediate model.

---
+++ models/intermediate/int_merged_history.sql
@@ -0,0 +1,72 @@
+with spotify as (
+
+    select
+        source_track_id,
+        spotify_track_id,
+        title_original,
+        artist_original,
+        album_original,
+        source,
+        release_year,
+        duration_ms,
+        duration_seconds,
+        popularity,
+        genres,
+        extraction_date
+    from {{ ref('stg_raw__spotify_history') }}
+
+),
+
+yt as (
+
+    select
+        track_id,
+        played_at,
+        ytm_url
+    from {{ ref('stg_raw__yt_history') }}
+
+),
+
+genre as (
+
+    select
+        spotify_raw_genre,
+        main_genre,
+        sub_genre
+    from {{ ref('stg_raw__genre') }}
+
+),
+
+joined as (
+
+    select
+        -- listening event grain
+        s.source_track_id AS yt_track_id,
+        y.played_at,
+
+        -- spotify metadata (source of truth)
+        s.spotify_track_id,
+        s.title_original         as title,
+        s.artist_original        as artist,
+        s.album_original         as album,
+        s.source,
+        s.release_year,
+        s.duration_ms,
+        s.duration_seconds,
+        s.popularity,
+        s.genres,
+        s.extraction_date,
+        y.ytm_url,
+        g.main_genre,
+        g.sub_genre
+
+
+    from spotify s
+    left join yt y
+        on y.track_id = s.source_track_id
+    left join genre g
+        on g.spotify_raw_genre = s.genres
+)
+
+select *
+from joined
